489751b03c1dbdf108c2fb6816b28770853ea836
Simon <simon@gb7fr.org.uk>
2022-02-19 00:22:13 +0000

    lkflx

--- config.py
+++ config.py
@@ -200,8 +200,8 @@ def build_config(_config_file):
                     'USER': config.get(section, 'USER'),
                     'PASS': config.get(section, 'PASS'),
                     'SERVER': config.get(section, 'SERVER'),
-                    'PORT': config.getint(section,'PORT'))
-                    'NODE' : config.getint(section,'NODE'))
+                    'PORT': config.getint(section,'PORT'),
+                    'NODE' : config.getint(section,'NODE')
             })
             
                 
