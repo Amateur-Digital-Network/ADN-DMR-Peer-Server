9a1b9f42bf7bffa89080daa7b534d269b4919141
Simon <simon@gb7fr.org.uk>
2022-05-21 20:06:22 +0100

    Fix column count

--- report_sql.py
+++ report_sql.py
@@ -91,7 +91,7 @@ class reportClient(NetstringReceiver):
         print("{} {} {} {} {} {} {} {} {} {} {}".format(event['type'],event['event'], event['trx'],event['system'],event['streamid'],event['peerid'],event['subid'],event['slot'],event['dstid'],event['duration'], event['source_server'],event['source_rptr']))
         _cursor = self.db.cursor()
         try:
-            _cursor.execute("insert into feed values (NULL,'{}','{}','{}','{}','{}','{}','{}','{}','{}','{},{},{}')".format(event['type'],event['event'], event['trx'],event['system'],event['streamid'],event['peerid'],event['subid'],event['slot'],event['dstid'],event['duration'],event['source_server'],event['source_rptr']))
+            _cursor.execute("insert into feed values (NULL,'{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{},{}')".format(event['type'],event['event'], event['trx'],event['system'],event['streamid'],event['peerid'],event['subid'],event['slot'],event['dstid'],event['duration'],event['source_server'],event['source_rptr']))
             self.db.commit()
         except mysql.connector.Error as err:
             _cursor.close()
