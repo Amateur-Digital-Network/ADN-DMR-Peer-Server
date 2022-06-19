e7224bfa1e6300bdfcb8d97dbd97fbeb2e01edd1
Simon <simon@gb7fr.org.uk>
2022-06-19 15:38:24 +0100

    If in

--- bridge_master.py
+++ bridge_master.py
@@ -687,7 +687,7 @@ def ident():
 
                 _dst_id = b''
                 
-                if CONFIG['SYSTEMS'][system]['OVERRIDE_IDENT_TG']:
+                if 'OVERRIDE_IDENT_TG' in CONFIG['SYSTEMS'][system] and CONFIG['SYSTEMS'][system]['OVERRIDE_IDENT_TG']:
                     _dst_id = bytes_3(OVERRIDE_IDENT_TG)
                 else:
                     _dst_id = _all_call
