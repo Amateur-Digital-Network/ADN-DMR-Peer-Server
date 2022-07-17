7903edce68eaf462ef181cb6c4540eb62a446028
Simon <simon@gb7fr.org.uk>
2022-07-17 01:15:52 +0100

    Explicitly send NAK when we timeout a peer

--- hblink.py
+++ hblink.py
@@ -769,6 +769,8 @@ class HBSYSTEM(DatagramProtocol):
                 remove_list.append(peer)
         for peer in remove_list:
             logger.info('(%s) Peer %s (%s) has timed out and is being removed', self._system, self._peers[peer]['CALLSIGN'], self._peers[peer]['RADIO_ID'])
+            #First, NAK the peer
+            self.transport.write(b''.join([MSTNAK, _peer_id]),self._CONFIG['SYSTEMS'][self._system]['PEERS'][peer]['SOCKADDR'])
             # Remove any timed out peers from the configuration
             del self._CONFIG['SYSTEMS'][self._system]['PEERS'][peer]
         if 'PEERS' not in self._CONFIG['SYSTEMS'][self._system] and 'OPTIONS' in self._CONFIG['SYSTEMS'][self._system]:
