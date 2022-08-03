32f70379732e4dd82eaa6b3603778582de8181b2
Simon <simon@gb7fr.org.uk>
2022-08-03 23:35:55 +0100

    dflkldk

--- hblink.py
+++ hblink.py
@@ -758,7 +758,7 @@ class OPENBRIDGE(DatagramProtocol):
                         _hops = _packet[13:14]
                         if _hash == _hash2:
                             logger.trace('(%s) *ProtoControl* BCTO received: %s connected to %s with proto ver. %s',self._system, int_id(_src), int_id(_dst), int.from_bytes(_ver,'big'))
-                            if (int(_hops.from_bytes('big')) < 10):
+                            if (int.from_bytes(_hops,'big') < 10):
                                 retransmit_bcto(_packet[:14])
                             else:
                                 if _hops > 10:
