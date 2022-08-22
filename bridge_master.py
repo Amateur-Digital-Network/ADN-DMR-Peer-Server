e4f84c47660fe981294354cb05ba4327466c2b9c
Simon <simon@gb7fr.org.uk>
2022-08-23 00:10:30 +0100

    GC stats and leak detection

--- bridge_master.py
+++ bridge_master.py
@@ -82,6 +82,11 @@ from binascii import b2a_hex as ahex
 
 from AMI import AMI
 
+#debugging and profiling
+import gc
+
+gc.set_debug(gc.DEBUG_STATS)
+gc.set_debug(gc.DEBUG_LEAK)
 
 ##from hmac import new as hmac_new, compare_digest
 ##from hashlib import sha256, hash
