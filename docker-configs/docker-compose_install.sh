8614390f23855bb1e808994e2cf89b7999b4be74
Simon <simon@gb7fr.org.uk>
2022-09-11 00:39:36 +0100

    jfksjy# with '#' will be ignored, and an empty message aborts the commit.

--- docker-configs/docker-compose_install.sh
+++ docker-configs/docker-compose_install.sh
@@ -28,12 +28,7 @@ echo Installing required packages...
 echo Install Docker Community Edition...
 #apt-get remove docker docker-engine docker.io &&
 apt-get -y update &&
-apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common &&
+apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common &&
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - &&
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
